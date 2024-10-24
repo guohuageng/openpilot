6b31581338f2689c3c55d963249196d05885481e
guohuageng <guohuageng@gmail.com>
2024-10-24 23:11:58 +0800

    更新私有服务端地址

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-export ATHENA_HOST='ws://opdiy.cn:5566'
-export API_HOST='http://opdiy.cn:7777'
+export ATHENA_HOST='ws://lirou.fun:7899'
+export API_HOST='http://lirou.fun:7898'
 export SKIP_FW_QUERY=1
 exec ./launch_chffrplus.sh
